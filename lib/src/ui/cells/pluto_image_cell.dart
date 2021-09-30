c6e9eeba389c76198a7ebecf7fe378e54b522520
Luko Gjenero <lgjenero@gmail.com>
2021-09-30 16:05:11 +0200

    Added handling for url/bytes futures

--- lib/src/ui/cells/pluto_image_cell.dart
+++ lib/src/ui/cells/pluto_image_cell.dart
@@ -23,20 +23,61 @@ class PlutoImageCell extends StatefulWidget implements AbstractMixinTextCell {
 class _PlutoImageCellState extends State<PlutoImageCell> {
   @override
   Widget build(BuildContext context) {
-    if (widget.column?.type?.isValid(widget.cell?.value) != true || widget.column?.type is! PlutoColumnTypeImage)
-      return Container();
+    if (widget.column?.type is! PlutoColumnTypeImage) return Container();
 
     PlutoColumnTypeImage type = widget.column!.type as PlutoColumnTypeImage;
 
     Widget content;
-    if (widget.cell?.value is Uint8List) {
+    if (widget.cell?.value is Future<Uint8List?> || widget.cell?.value is Future<Uint8List>) {
+      Future<Uint8List?> future;
+      if (widget.cell?.value is Future<Uint8List?>) {
+        future = widget.cell?.value as Future<Uint8List?>;
+      } else {
+        future = widget.cell?.value as Future<Uint8List>;
+      }
+
+      content = FutureBuilder<Uint8List?>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.data != null) {
+            return Image.memory(snapshot.data!, fit: type.fit ?? BoxFit.cover);
+          } else {
+            return ColoredBox(color: Colors.grey.shade100);
+          }
+        },
+      );
+    } else if (widget.cell?.value is Future<String?> || widget.cell?.value is Future<String>) {
+      Future<String?> future;
+      if (widget.cell?.value is Future<String?>) {
+        future = widget.cell?.value as Future<String?>;
+      } else {
+        future = widget.cell?.value as Future<String>;
+      }
+
+      content = FutureBuilder<String?>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.data != null) {
+            if (type.isAsset) {
+              return Image.asset(snapshot.data!, fit: type.fit ?? BoxFit.cover);
+            } else {
+              return Image.network(snapshot.data!, fit: type.fit ?? BoxFit.cover);
+            }
+          } else {
+            return ColoredBox(color: Colors.grey.shade100);
+          }
+        },
+      );
+    } else if (widget.cell?.value is Uint8List) {
       content = Image.memory(widget.cell!.value as Uint8List, fit: type.fit ?? BoxFit.cover);
-    } else {
+    } else if (widget.cell?.value is String) {
       if (type.isAsset) {
         content = Image.asset(widget.cell!.value as String, fit: type.fit ?? BoxFit.cover);
       } else {
         content = Image.network(widget.cell!.value as String, fit: type.fit ?? BoxFit.cover);
       }
+    } else {
+      content = Container();
     }
 
     if (type.decoration != null)
