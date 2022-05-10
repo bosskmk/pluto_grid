a078b3dd6a621cf2b3ad1db7ac916b3b546335eb
kmk <manekingz@gmail.com>
2022-05-10 21:19:31 +0900

    Fixed url_launcher.

--- demo/lib/helper/launch_url.dart
+++ demo/lib/helper/launch_url.dart
@@ -1,7 +1,7 @@
 import 'package:url_launcher/url_launcher.dart';
 
 void launchUrl(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
+  if (await canLaunchUrl(Uri.parse(url))) {
+    launchUrl(url);
   }
 }
