6d1e2249d78ff21a3c5b194bcaef16b209fe51e1
kmk <manekingz@gmail.com>
2023-05-11 06:50:45 +0900

    Compatible with Flutter 3.10

--- demo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ demo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import printing
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
