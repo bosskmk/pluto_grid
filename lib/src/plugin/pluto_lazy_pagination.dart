8e54396b4ccc2b1eee0c02e97c73fd9bf6f6d6a1
Sokcevic <40267209+Sokcevic@users.noreply.github.com>
2024-04-12 10:01:44 +0200

    Update pluto_lazy_pagination.dart, Check if bodyRowsVertical has any Clients to prevent Exceptions caused by async events

--- lib/src/plugin/pluto_lazy_pagination.dart
+++ lib/src/plugin/pluto_lazy_pagination.dart
@@ -180,6 +180,7 @@ class _PlutoLazyPaginationState extends State<PlutoLazyPagination> {
       ),
     )
         .then((data) {
+      if(!stateManager.scroll.bodyRowsVertical!.hasClients)return;
       stateManager.scroll.bodyRowsVertical!.jumpTo(0);
 
       stateManager.refRows.clearFromOriginal();
